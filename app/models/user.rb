eeb8b794ed65b888c47803832748c4f951780a01
congson0710 <son.trinh@employmenthero.com>
2018-07-09 11:21:27 +0700

    validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: {minumum: 5, maximum: 100}
-  validates :email, presence: true, length: {minumum: 5, maximum: 100}
+  validates :username, presence: true,
+                       length: {minumum: 5, maximum: 100},
+                       uniqueness: {case_sensitive: false}
+                       VALID_EMAIL_REGEX: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    length: {minumum: 5, maximum: 100},
+                    uniqueness: {case_sensitive: false},
+                    format: {with: VALID_EMAIL_REGEX}
 end
